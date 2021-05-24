e0c63b608cf2d26fc5b9fd6772e946cc592b624b
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2021-05-24 08:26:52 +0200

    first

---
+++ src/icons.js
@@ -0,0 +1,34 @@
+/*export const ikonyglowne = {
+    first: "./images/Dopalacze1_Internet.jpg",
+    firtscliced: "./images/Dopalacze1_Internet_clicked.jpg",
+    second: "./images/Dopalacze2_Internet.jpg",
+    secondcliced: "./images/Dopalacze2_Internet_clicked.jpg",
+    third: "./images/Dopalacze3_Internet.jpg",
+    thirdcliced: "./images/Dopalacze3_Internet_clicked.jpg",
+    home:"./images/home.png",
+    homeclicked:"./images/home_clicked.png"
+}*/
+
+export const iconA = new Image();
+iconA.src="./images/Dopalacze1_Internet.jpg";
+
+export const iconAclicked = new Image();
+iconAclicked.src="./images/Dopalacze1_Internet_clicked.jpg";
+
+export const iconB = new Image();
+iconB.src ="./images/Dopalacze2_Internet.jpg";
+
+export const iconBclicked = new Image();
+iconBclicked.src="./images/Dopalacze2_Internet_clicked.jpg";
+
+export const iconC = new Image();
+iconC.src = "./images/Dopalacze3_Internet.jpg";
+
+export const iconCclicked = new Image();
+iconCclicked.src = "./images/Dopalacze3_Internet_clicked.jpg"
+
+export const homeButton = new Image();
+homeButton.src = "./images/home.png";
+
+export const homeButtonclicked = new Image();
+homeButtonclicked.src = "./images/home_clicked.png"
